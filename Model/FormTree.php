8bcef5878163ff8a8b7eec37d8890e00f87f58f7
tom32i <thomas.jarrand@gmail.com>
2014-01-25 10:10:39 +0100

    Array Access interface + space indentation

--- Model/FormTree.php
+++ Model/FormTree.php
@@ -15,107 +15,152 @@ namespace Elao\Bundle\FormTranslationBundle\Model;
  *
  * @author Thomas Jarrand <thomas.jarrand@gmail.com>
  */
-class FormTree implements \Iterator, \Countable
+class FormTree implements \Iterator, \Countable, \ArrayAccess
 {
-	/**
-	 * The FormTreeNode elements
-	 *
-	 * @var array
-	 */
-	private $nodes;
+    /**
+     * The FormTreeNode elements
+     *
+     * @var array
+     */
+    private $nodes;
 
-	/**
-	 * Current position in the loop
-	 *
-	 * @var integer
-	 */
-	private $position = 0;
+    /**
+     * Current position in the loop
+     *
+     * @var integer
+     */
+    private $position = 0;
 
-	/**
-	 * Constructor
-	 */
-	public function __construct($nodes = array())
-	{
-		$this->nodes = $nodes;
-	}
+    /**
+     * Constructor
+     */
+    public function __construct($nodes = array())
+    {
+        $this->nodes = $nodes;
+    }
 
-	/**
-	 * Add a parent node to the beginning of the tree
-	 *
-	 * @param FormTreeNode $node The node
-	 *
-	 * @return int The new number of elements in the Tree
-	 */
-	public function addParent(FormTreeNode $node)
-	{
-		return array_unshift($this->nodes, $node);
-	}
+    /**
+     * Add a parent node to the beginning of the tree
+     *
+     * @param FormTreeNode $node The node
+     *
+     * @return int The new number of elements in the Tree
+     */
+    public function addParent(FormTreeNode $node)
+    {
+        return array_unshift($this->nodes, $node);
+    }
 
-	/**
-	 * Add a child node to the end of the tree
-	 *
-	 * @param FormTreeNode $node The node
-	 *
-	 * @return int The new number of elements in the Tree
-	 */
-	public function addChild(FormTreeNode $node)
-	{
-		return array_push($this->nodes, $node);
-	}
+    /**
+     * Add a child node to the end of the tree
+     *
+     * @param FormTreeNode $node The node
+     *
+     * @return int The new number of elements in the Tree
+     */
+    public function addChild(FormTreeNode $node)
+    {
+        return array_push($this->nodes, $node);
+    }
 
-	/**
-	 * Set the loop back to the start
-	 */
-	public function rewind()
-	{
-		$this->position = 0;
-	}
+    /**
+     * Set the loop back to the start
+     */
+    public function rewind()
+    {
+        $this->position = 0;
+    }
 
-	/**
-	 * Return the length of the tree
-	 *
-	 * @return int
-	 */
-	public function count()
-	{
-		return count($this->nodes);
-	}
+    /**
+     * Return the length of the tree
+     *
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->nodes);
+    }
 
-	/**
-	 * Return the current Node in the loop
-	 *
-	 * @return FormTreeNode
-	 */
-	public function current()
-	{
-		return $this->nodes[$this->position];
-	}
+    /**
+     * Return the current Node in the loop
+     *
+     * @return FormTreeNode
+     */
+    public function current()
+    {
+        return $this->nodes[$this->position];
+    }
 
-	/**
-	 * Return the current position in the loop
-	 *
-	 * @return int
-	 */
-	public function key()
-	{
-		return $this->position;
-	}
+    /**
+     * Return the current position in the loop
+     *
+     * @return int
+     */
+    public function key()
+    {
+        return $this->position;
+    }
 
-	/**
-	 * Increment current position
-	 */
-	public function next()
-	{
-		++$this->position;
-	}
+    /**
+     * Increment current position
+     */
+    public function next()
+    {
+        ++$this->position;
+    }
 
-	/**
-	 * Return weither or not the current position is valid
-	 *
-	 * @return int
-	 */
-	public function valid()
-	{
-		return isset($this->nodes[$this->position]);
-	}
+    /**
+     * Return weither or not the current position is valid
+     *
+     * @return int
+     */
+    public function valid()
+    {
+        return $this->offsetExists($this->position);
+    }
+
+    /**
+     * Return weither or not the given offset exists
+     *
+     * @param mixed $offset
+     *
+     * @return boolean
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->nodes[$offset]);
+    }
+
+    /**
+     * Get the node at the given offset
+     *
+     * @param mixed $offset
+     *
+     * @return FormTreeNode
+     */
+    public function offsetGet($offset)
+    {
+        return $this->offsetExists($offset) ? $this->nodes[$offset] : null;
+    }
+
+    /**
+     * Set the node at the given offset
+     *
+     * @param mixed $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value)
+    {
+        /* Not implemented: User addParent and addChild methods */
+    }
+
+    /**
+     * Unset node at the given offset
+     *
+     * @param mixed $offset
+     */
+    public function offsetUnset($offset)
+    {
+        /* Not implemented: FormTree nodes should not be unsetable */
+    }
 }
--- Model/FormTreeNode.php
+++ Model/FormTreeNode.php
@@ -17,87 +17,87 @@ namespace Elao\Bundle\FormTranslationBundle\Model;
  */
 class FormTreeNode
 {
-	/**
-	 * The name of the node
-	 *
-	 * @var string
-	 */
-	private $name;
+    /**
+     * The name of the node
+     *
+     * @var string
+     */
+    private $name;
 
-	/**
-	 * Weither or not the node has labeled children.
-	 *
-	 * @var boolean
-	 */
-	private $children;
+    /**
+     * Weither or not the node has labeled children.
+     *
+     * @var boolean
+     */
+    private $children;
 
-	/**
-	 * Weither or not the node is a collection.
-	 *
-	 * @var boolean
-	 */
-	private $collection;
+    /**
+     * Weither or not the node is a collection.
+     *
+     * @var boolean
+     */
+    private $collection;
 
-	/**
-	 * Weither or not the node is a prototype.
-	 *
-	 * @var boolean
-	 */
-	private $prototype;
+    /**
+     * Weither or not the node is a prototype.
+     *
+     * @var boolean
+     */
+    private $prototype;
 
-	/**
-	 * Constructor
-	 *
-	 * @param string  $name       The node's name
-	 * @param boolean $children   Weither or not the node has labeled children
-	 * @param boolean $collection Is the node a collection
-	 * @param boolean $prototype  Is the node a prototype
-	 */
-	public function __construct($name, $children = false, $collection = false, $prototype = false)
-	{
-		$this->name       = (string) $name;
-		$this->children   = (boolean) $children;
-		$this->collection = (boolean) $collection;
-		$this->prototype  = (boolean) $prototype;
-	}
+    /**
+     * Constructor
+     *
+     * @param string  $name       The node's name
+     * @param boolean $children   Weither or not the node has labeled children
+     * @param boolean $collection Is the node a collection
+     * @param boolean $prototype  Is the node a prototype
+     */
+    public function __construct($name, $children = false, $collection = false, $prototype = false)
+    {
+        $this->name       = (string) $name;
+        $this->children   = (boolean) $children;
+        $this->collection = (boolean) $collection;
+        $this->prototype  = (boolean) $prototype;
+    }
 
-	/**
-	 * Get the name of the node
-	 *
-	 * @return string
-	 */
-	public function getName()
-	{
-		return $this->name;
-	}
+    /**
+     * Get the name of the node
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 
-	/**
-	 * Does the node has labeled children?
-	 *
-	 * @return boolean
-	 */
-	public function hasChildren()
-	{
-		return $this->children;
-	}
+    /**
+     * Does the node has labeled children?
+     *
+     * @return boolean
+     */
+    public function hasChildren()
+    {
+        return $this->children;
+    }
 
-	/**
-	 * Is the node a collection?
-	 *
-	 * @return boolean
-	 */
-	public function isCollection()
-	{
-		return $this->collection;
-	}
+    /**
+     * Is the node a collection?
+     *
+     * @return boolean
+     */
+    public function isCollection()
+    {
+        return $this->collection;
+    }
 
-	/**
-	 * Is the node a prototype?
-	 *
-	 * @return boolean
-	 */
-	public function isPrototype()
-	{
-		return $this->prototype;
-	}
+    /**
+     * Is the node a prototype?
+     *
+     * @return boolean
+     */
+    public function isPrototype()
+    {
+        return $this->prototype;
+    }
 }
